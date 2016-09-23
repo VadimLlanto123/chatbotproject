1dd7fbba181ef61b70935043ba4ff5080da30eb0
Vadim Llanto <vadillano69@gmail.com>
2016-09-23 09:13:28 -0700

    Minor changes to the reactions

--- chatbot.js
+++ chatbot.js
@@ -1,6 +1,6 @@
 function talkButton() {
   var objDiv = document.getElementById("chat-area");
-  var greetResponse = ["Hello", "How's it goin'?", "What is it now?", "What's up?", "Greetings friend", "Yeah, hi", "I greet you", "suhhhh duuuu", "ey bawss"]
+  var greetResponse = ["Hello", "How's it goin'", "What is it now", "Hey it's Valdeen, I'm heeeere", "What's up", "Greetings friend", "Yeah, hi", "I greet you", "suhhhh duuuu", "ey bawss", "Hey you guyyyyys!"]
   var randomResponse = Math.floor(Math.random() * 6) + 0;
   var currentdate = new Date();
   var hours = currentdate.getHours();
@@ -12,8 +12,8 @@ function talkButton() {
   }
   var datetime = "It is currently " + hours + ":" + currentdate.getMinutes();
   var response = {
-    "how old are you": "legal",
-    "how old are you?": "legal",
+    "how old are you": "legal age",
+    "how old are you?": "legal age",
     "hey": greetResponse[randomResponse],
     "hello": greetResponse[randomResponse],
     "hi": greetResponse[randomResponse],
@@ -21,28 +21,30 @@ function talkButton() {
     "hello Vadim": greetResponse[randomResponse],
     "greetings": greetResponse[randomResponse],
     "what is your name": "Vadim (D A D D I) Llanto",
-    "whats your name": "Vadim (D A D D I) Llanto",
+    "whats your name": "Names are only labels created by man",
     "what time is it": datetime,  "whats the time": datetime,
     "where are you from": "Your window",
-    "what is your favorite color": "blue",
-    "what is your favorite food": "Big mac",
-    "how are you": "Better than you",
-    "what do you mean": "When you nod your head yes",
-    "what is your gender": "UNIDENTIFIED",
-    "when is your birthday": "about a week agooooo",
-    "why": "Why not",
-    "i hate you": "Love you too",
-    "i like you": "I already knew that",
-    "i love you": "Sorry hun, I'm taken",
+    "what is your favorite color": "What are we in Kindergarten?",
     "what color is your hair": "Blue",
-    "who am i": "That is... A good question...",
-    "what are you": "Whatever I want to be hun",
+    "what is your favorite food": "Big mac",
+    "how are you": "My answer is too complex for you",
+    "i dont care": "Well, that makes two of us",
+    "who am i": "That is a good question",
+    "what are you": "Whatever I want to be, hun",
+    "what do you mean": "When you nod your head yes",
+    "what is your gender": "Yes all of the below",
+    "when is your birthday": "About a week agooooo",
+    "why": "Why not",
+    "i hate you": "Love you too bae <3",
+    "i like you": "There is no need to state the obvious",
+    "i love you": "Go home human, you're drunk",
+    "i want you": "I'm flattered",
     "who are you": "Your mother NOW GO CLEAN YOUR ROOM",
     "what is the meaning of life": "42",
-    "i dont care": "Same",
+
   }
 
-  var nonsense = ["No thanks I'm not interested", "K den weirdo", "Wowowowowowow", "R O O D", "Give it up", "Y u do dis?", "Ugh... Mortals", "Probably", "Not up for discussion", "Lose it", "...", "Cosa vol dire", "You sicken me", "I knew I should have gone to Mars"]
+  var nonsense = ["No thanks I'm not interested", "K den weirdo", "Wowowowowowow", "yeahhhhh... whatever that means","R O O D", "Give it up", "Y u do dis?", "Ugh... Mortals", "Probably", "Not up for discussion", "Lose it", "...", "Cosa vol dire", "You sicken me", "whatever you say, bawss" "I knew I should have gone to Mars"]
   var random = Math.floor(Math.random() * 13) + 0;
 
   //document.getElementById('chat-area').textContent = document.getElementById('input').value;
