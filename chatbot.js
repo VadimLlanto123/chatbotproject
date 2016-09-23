7c5b2284f73910483e461772222b6f6ae2b5951c
Vadim Llanto <vadillano69@gmail.com>
2016-09-23 09:26:36 -0700

    Changes to the reactions

--- chatbot.js
+++ chatbot.js
@@ -21,7 +21,7 @@ function talkButton() {
     "hello Vadim": greetResponse[randomResponse],
     "greetings": greetResponse[randomResponse],
     "what is your name": "Vadim (D A D D I) Llanto",
-    "whats your name": "Names are only labels created by man",
+    "whats your name": "Names are only labels created by man, I am above this meta physical plane",
     "what time is it": datetime,  "whats the time": datetime,
     "where are you from": "Your window",
     "what is your favorite color": "What are we in Kindergarten?",
@@ -29,22 +29,22 @@ function talkButton() {
     "what is your favorite food": "Big mac",
     "how are you": "My answer is too complex for you",
     "i dont care": "Well, that makes two of us",
-    "who am i": "That is a good question",
+    "who am i": "That is... A good question...",
     "what are you": "Whatever I want to be, hun",
-    "what do you mean": "When you nod your head yes",
-    "what is your gender": "Yes all of the below",
+    "what do you mean": "When you nod your head yes. but you wanna say noooo",
+    "what is your gender": "Yes; all of the below",
     "when is your birthday": "About a week agooooo",
-    "why": "Why not",
+    "why": "Why not?",
     "i hate you": "Love you too bae <3",
     "i like you": "There is no need to state the obvious",
     "i love you": "Go home human, you're drunk",
     "i want you": "I'm flattered",
-    "who are you": "Your mother NOW GO CLEAN YOUR ROOM",
+    "who are you": "Your mother.... NOW GO CLEAN YOUR ROOM",
     "what is the meaning of life": "42",
 
   }
 
-  var nonsense = ["No thanks I'm not interested", "K den weirdo", "Wowowowowowow", "yeahhhhh... whatever that means","R O O D", "Give it up", "Y u do dis?", "Ugh... Mortals", "Probably", "Not up for discussion", "Lose it", "...", "Cosa vol dire", "You sicken me", "whatever you say, bawss" "I knew I should have gone to Mars"]
+  var nonsense = ["No thanks I'm not interested", "K den weirdo", "Wowowowowowow", "yeahhhhh... whatever that means","R O O D", "Give it up", "Y u do dis?", "Ugh... Mortals", "Probably", "Not up for discussion", "Lose it", "...", "Cosa vol dire", "You sicken me", "whatever you say, bawss","I knew I should have gone to Mars"]
   var random = Math.floor(Math.random() * 13) + 0;
 
   //document.getElementById('chat-area').textContent = document.getElementById('input').value;
